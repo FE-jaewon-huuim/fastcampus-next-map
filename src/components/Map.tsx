ccac04fa3717d6e1b2dfe4cc1f34e2b39517252b
이재원 <jenyglee_30@naver.com>
2023-12-06 18:00:03 +0900

    중간저장

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,53 +1,42 @@
 /*global kakao*/
-import Script from "next/script";
-import * as stores from "@/data/store_data.json";
+import Script from 'next/script';
+import * as stores from '@/data/store_data.json';
+import { Dispatch, SetStateAction } from 'react';
 
 declare global {
-  interface Window {
-    kakao: any;
-  }
+    interface Window {
+        kakao: any;
+    }
 }
 
 const DEFAULT_LAT = 37.497625203;
 const DEFAULT_LNG = 127.03088379;
 
-export default function Map() {
-  const loadKakaoMap = () => {
-    window.kakao.maps.load(() => {
-      const mapContainer = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-      const mapOption = {
-        //지도를 생성할 때 필요한 기본 옵션
-        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG), //지도의 중심좌표
-        level: 3, //지도의 레벨(확대, 축소 정도)
-      };
-      const map = new window.kakao.maps.Map(mapContainer, mapOption);
+interface MapProps {
+    setMap: Dispatch<SetStateAction<any>>;
+}
 
-      // 식당 데이터 마커 띄우기
-      stores?.["DATA"]?.map((store) => {
-        // 마커가 표시될 위치입니다
-        var markerPosition = new window.kakao.maps.LatLng(
-          store?.y_dnts,
-          store?.x_cnts
-        );
-
-        // 마커를 생성합니다
-        var marker = new window.kakao.maps.Marker({
-          position: markerPosition,
+export default function Map({ setMap }: MapProps) {
+    const loadKakaoMap = () => {
+        window.kakao.maps.load(() => {
+            const mapContainer = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+            const mapOption = {
+                //지도를 생성할 때 필요한 기본 옵션
+                center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG), //지도의 중심좌표
+                level: 3, //지도의 레벨(확대, 축소 정도)
+            };
+            const map = new window.kakao.maps.Map(mapContainer, mapOption);
+            setMap(map);
         });
-
-        // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map);
-      });
-    });
-  };
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
-        onReady={loadKakaoMap}
-      ></Script>
-      <div id="map" className="w-full h-screen"></div>
-    </>
-  );
+    };
+    return (
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+                onReady={loadKakaoMap}
+            ></Script>
+            <div id="map" className="w-full h-screen"></div>
+        </>
+    );
 }
